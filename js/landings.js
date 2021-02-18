21d85a1248fb1ee2e6d78941ffa77eaddd51bd36
Andrea <andrea.crucesdelaroca@gmail.com>
2021-02-18 03:20:17 -0500

    cambios en internacionalidad

--- js/landings.js
+++ js/landings.js
@@ -30,67 +30,25 @@ $(document).ready(function () {
     });
 
 
+    // Tabs countries //
 
+    $('.tab-list li a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
 
-
-
-
-
-
-
-
-    $('body').on('click', '.close-convenio', function (e) {
-		$('.convenio-univ-toggle-wrap:visible').removeClass('active').stop().slideUp();
-	});
-
-	$('body').on('click', '.section-country .close-convenio', function (e) {
-		$('.section-country .convenio-univ-toggle-wrap:visible').removeClass('active').stop().slideUp();
-	});
-
-	$('body').on('click', '.convenio-toggle', function (e) {
-		e.preventDefault();
-
-		var getMargin = $(this).parent().height() - $(this).find('.speaker-image-circle').height();
-
-		var speakerId = $(this).attr('data-convenio-id');
-
-		$('.convenio-univ-toggle-wrap[data-convenio-id=' + speakerId + ']').css('margin-top', '-' + getMargin + 'px');
-		$('.convenio-univ-toggle-wrap:visible').stop().slideUp();
-
-		var curSpeakerId = $('.convenio-univ-toggle-wrap:visible').attr('data-convenio-id');
-
-		$('.convenio-toggle').removeClass('active');
-		if ($('.convenio-univ-toggle-wrap[data-convenio-id=' + speakerId + ']').is(':visible')) {
-
-			$('.convenio-univ-toggle-wrap[data-convenio-id=' + speakerId + ']').stop().slideUp();
-			$(this).removeClass('active');
-			//$(this).find('.mobile-plus-icon-large').show();
-		} else {
-			$(this).addClass('active');
-			$('.convenio-univ-toggle-wrap[data-convenio-id=' + speakerId + ']').stop().slideDown();
-			//$('.convenio-toggle[data-convenio-id=' + curSpeakerId + ']').find('.mobile-plus-icon-large').show();
-			//$(this).find('.mobile-plus-icon-large').hide();
-		}
+    $('a.scroll').on('click', function (e) {
+        var href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top -58
+        }, 'slow');
+        e.preventDefault();
     });
 
-    
-    
-
-    $('body').on('click', '.mobile-convenio-toggle', function (e) {
-		//if ($(e.target).hasClass('bio-toggle')) return;
-		var clickedSpeakerBio = $(this).find('.convenio-info .convenio-univ-mobile');
-		$('.mobile-convenio-toggle.active').find('.convenio-info .convenio-univ-mobile').slideUp();
-		if ($(this).hasClass('active')) {
-			$(this).removeClass('active');
-		} else {
-			$('.mobile-convenio-toggle.active').removeClass('active');
-			$(clickedSpeakerBio).slideDown();
-			$(this).addClass('active');
-		}
-	});
-
-
-    
+    $(".close-tab").click(function () {
+        $(".tab-list li").removeClass("active");
+        $(".active .tab-container").addClass("close-content");
+    });
 
 
 });
@@ -103,14 +61,14 @@ document.querySelectorAll('.dropdown-toggle').forEach(dropDownFunc);
 function dropDownFunc(dropDown) {
     console.log(dropDown.classList.contains('click-dropdown'));
 
-    if(dropDown.classList.contains('click-dropdown') === true){
+    if (dropDown.classList.contains('click-dropdown') === true) {
         dropDown.addEventListener('click', function (e) {
-            e.preventDefault();        
-    
+            e.preventDefault();
+
             if (this.nextElementSibling.classList.contains('dropdown-active') === true) {
                 this.parentElement.classList.remove('dropdown-open');
                 this.nextElementSibling.classList.remove('dropdown-active');
-    
+
             } else {
                 closeDropdown();
                 this.parentElement.classList.add('dropdown-open');
